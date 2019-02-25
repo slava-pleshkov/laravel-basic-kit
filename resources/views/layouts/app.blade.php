2ccf3b910ecfe9e9a43b415a6c04053fee978f5c
Slava Pleshkov <mail@slavapleshkov.com>
2019-02-25 13:15:38 +0200

    Add heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,18 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('icons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('icons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('icons/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('icons/safari-pinned-tab.svg') }}" color="#ff0000">
+    <link rel="shortcut icon" href="{{ asset('icons/favicon.ico') }}">
+    <meta name="msapplication-TileColor" content="#ffffff') }}">
+    <meta name="msapplication-TileImage" content="{{ asset('icons/mstile-144x144.png') }}">
+    <meta name="msapplication-config" content="{{ asset('icons/browserconfig.xml') }}">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
     <div id="app">
